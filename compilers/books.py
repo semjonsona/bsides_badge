5a6aca41d8b148dd2136bad969be51ac5a516fa5
Semjon/Sona Kravtsenko <semjon.00@gmail.com>
2025-09-28 15:07:30 +0300

    Better books compiler

--- compilers/books.py
+++ compilers/books.py
@@ -1,46 +1,71 @@
 import os
-import string
 import struct
 import bs4
+import re
 
-TITLE_SIZE = 64
+import util
 
 def text_encode(txt):
-    txt = ''.join([c for c in txt if c in """!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ \n"""])
+    def insert_breaks_in_ascii_letters(text: str, chunk: int = 16) -> str:
+        pattern = rf'[A-Za-z\-\_\?\^]{{{chunk + 1},}}'
+        def repl(m):
+            s = m.group(0)
+            parts = [s[i:i + chunk] for i in range(0, len(s), chunk)]
+            return '--\n'.join(parts)
+        return re.sub(pattern, repl, text)
+
+    txt = util.prepare_text(txt, fallback='_', loud=True)
+    txt = insert_breaks_in_ascii_letters(txt)
     return txt.encode('ascii')
 
+def with_length(obj):
+    return struct.pack("<I", len(obj)) + obj
+
+def compile(obj):
+    if isinstance(obj, str):
+        return text_encode(obj)
+    elif isinstance(obj, list):  # e.g. top level books nav is this
+        sb = bytearray()
+        for el in obj:
+            assert isinstance(el, tuple) and len(el) == 2 and isinstance(el[0], str)
+            el_type = b'0' if isinstance(el[1], list) else b'1' if isinstance(el[1], str) else None
+            assert el_type is not None
+            sb += el_type
+            sb += with_length(compile(el[0]))
+            sb += with_length(compile(el[1]))
+        return sb
+    else:
+        raise Exception('Incorrect hierarchy')
+
+
 if __name__ == '__main__':
-    bb = bytearray()
+    books = []
     for fn in os.listdir('books'):
-        print(fn)
+        print('Processing', fn)
         if not fn.lower().endswith(f".html"):
             continue
-        s = bs4.BeautifulSoup(open('books/' + fn))
+        s = bs4.BeautifulSoup(open('books/' + fn), features="lxml")
         chapters = []
         brief = s.find_all('header')[0].text.strip('\n').replace('\n', '\n\n')
         book_name = brief.split('\n')[0]
         chapters.append(('= Description =', brief))
-        for article in s.find_all('article'):
-            title = article.find('h1').text
-            text = ""
-            for p in article.find_all('p')[1:-1]:
-                text += p.text
-                text += '\n\n'
-            chapters.append((title, text))
+        for chapter in s.find_all('article'):
+            title = chapter.find('h1').text
+            texts = [""]
+            for p in chapter.find_all('p')[1:-1]:
+                texts[-1] += p.text
+                if len(texts[-1]) < 10000:
+                    texts[-1] += '\n\n'
+                else:
+                    texts.append("")
+            if len(texts) == 1:
+                texts = texts[0]
+            else:
+                ln = len(texts)
+                texts = [(f'{i+1}/{ln}', text) for i, text in enumerate(texts)]
+            chapters.append((title, texts))
+        books.append((book_name, chapters))
 
-        sb = bytearray()
-        book_name = text_encode(book_name)
-        for name, text in chapters:
-            name = text_encode(name)
-            text = text_encode(text)
-            sb += b'1'  # type text
-            sb += struct.pack("<I", len(name))
-            sb += name
-            sb += struct.pack("<I", len(text))
-            sb += text
-        bb += b'0'  # type book
-        bb += struct.pack("<I", len(book_name))
-        bb += book_name
-        bb += struct.pack("<I", len(sb))
-        bb += sb
+    bb = compile(books)
+    print('books.bin size: ', len(bb))
     open('books.bin', 'wb').write(bb)
---
+++ compilers/util.py
@@ -0,0 +1,10 @@
+CHARSET = """\n !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+TRANSITIONS = {'“': '"', '”': '"', '…': '...', "’": "'", "‘": "'", "—": '---', 'á': "a'", 'é': "e'", 'ï': 'ii',
+               'ç': 'c,', '№': 'No', 'â': 'a', 'è': 'e`'}
+
+def prepare_text(txt, fallback='', loud=False):
+    if loud:
+        unk = [c for c in list(set(txt)) if c not in CHARSET and c not in TRANSITIONS]
+        if len(unk) != 0:
+            print('unknown characters:', [(u, u.encode().hex()) for u in unk])
+    return ''.join(c if c in CHARSET else TRANSITIONS[c] if c in TRANSITIONS else fallback for c in txt)
